e6bf1e249ed82f65f644c2dc03be8da30c398200
pabloMaster123 <91347621+pabloMaster123@users.noreply.github.com>
2024-11-06 14:34:46 -0500

    Following enemies

---
+++ Scripts/shieldman.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var SPEED = 40
+var player_chase = false
+var player = null
+
+func _physics_process(delta: float) -> void:
+	if player_chase:
+		position += (player.position - position)/SPEED
+		$AnimatedSprite2D.play("walk")
+		
+		if(player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.play("default")	
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	player = body
+	player_chase = true
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	player = null
+	player_chase = false
