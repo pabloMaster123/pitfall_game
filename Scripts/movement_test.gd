2f66cda6519d290b0d853bbfd54ac06e5b0a3026
pabloMaster123 <91347621+pabloMaster123@users.noreply.github.com>
2024-11-08 12:30:53 -0500

    Attack animations and hitboxes

--- Scripts/movement_test.gd
+++ Scripts/movement_test.gd
@@ -1,8 +1,22 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
+var enemy_in_range = true
+var enemy_in_attack_range = false
+var enemy_cooldown = true
+
+@onready var attack_cooldown: Timer = $attack_cooldown
+
+var health = 200
+var is_alive = true
+
+const SPEED = 400.0
 const ACCELERATION = 1500
-const FRICTION = 600
+const FRICTION = 1000
+
+var current_direction = Vector2.ZERO
+var current_animation = ""
+
+
 
 func player_movement(input, delta):
 	if input: velocity = velocity.move_toward(input * SPEED , delta * ACCELERATION)
@@ -11,4 +25,63 @@ func player_movement(input, delta):
 func _physics_process(delta):
 	var input = Input.get_vector("left","rigth","up","down")
 	player_movement(input, delta)
+	play_direction_anim(input, delta)
 	move_and_slide()
+	enemy_attack(delta)
+	
+	if health <= 0:
+		is_alive = false
+		health = 0
+		print("The Hero is death")
+		self.queue_free()
+
+func play_direction_anim(input, delta):
+	var anim = $AnimatedSprite2D
+
+	var new_animation = ""
+
+	if input.x != 0 or input.y != 0:
+		current_direction = input
+
+	if current_direction.x > 0 and velocity.length() > 0:
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("run")
+	elif current_direction.x < 0 and velocity.length() > 0:
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("run")
+		current_animation = "run"
+	elif current_direction.y > 0 and velocity.length() > 0:
+		$AnimatedSprite2D.play("down_runing")
+		current_animation = "down_runing"
+	elif current_direction.y < 0 and velocity.length() > 0:
+		$AnimatedSprite2D.play("up_runing")
+		current_animation = "up_runing"
+
+	else:
+		if current_animation.ends_with("run") and velocity.length() == 0:
+			$AnimatedSprite2D.play("default")
+		elif current_animation.ends_with("down_runing") and velocity.length() == 0:
+			$AnimatedSprite2D.play("down")
+		elif current_animation.ends_with("up_runing") and velocity.length() == 0:
+			$AnimatedSprite2D.play("up")
+
+func  player():
+	pass
+
+func _on_player_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_in_attack_range = true
+
+func _on_player_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_in_attack_range = false # Replace with function body.
+
+func enemy_attack(delta):
+	if enemy_in_attack_range and enemy_cooldown == true:
+		health -= 20
+		enemy_cooldown = false
+		attack_cooldown.start()
+		print(health)
+	
+func _on_attack_cooldown_timeout() -> void:
+	enemy_cooldown = true # Replace with function body.
--- Scripts/shieldman.gd
+++ Scripts/shieldman.gd
@@ -1,10 +1,18 @@
 extends CharacterBody2D
 
-var SPEED = 40
+var SPEED = 30
 var player_chase = false
 var player = null
 
+var health = 400
+var player_inattack_zone = false
+
+var attack_ip = false
+
+
 func _physics_process(delta: float) -> void:
+	deal_with_damage()
+	
 	if player_chase:
 		position += (player.position - position)/SPEED
 		$AnimatedSprite2D.play("walk")
@@ -14,7 +22,8 @@ func _physics_process(delta: float) -> void:
 		else:
 			$AnimatedSprite2D.flip_h = false
 	else:
-		$AnimatedSprite2D.play("default")	
+		$AnimatedSprite2D.play("default")
+		
 
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
@@ -24,3 +33,23 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false
+
+func enemy(delta):
+	pass
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = true# Replace with function body.
+
+func _on_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = false # Replace with function body.
+
+func deal_with_damage():
+	if player_inattack_zone and Global.player_current_attack == true:
+		health -= 20
+		print("enemy health -" , health)
+		if health <= 0:
+			self.queue_free()
+		
+		
